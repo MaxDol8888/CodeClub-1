d308946eebdb11d88655e60917f7f1c1a8197754
Casey Rubano <crubano@paypal.com>
2013-04-04 11:59:13 -0700

    Added one liner soln

--- python/euler1.py
+++ python/euler1.py
@@ -1,10 +1,12 @@
+###############################################################################
 # Euler 1
 # http://projecteuler.net/index.php?section=problems&amp;id=1
+#
 # If we list all the natural numbers below 10 that are
 # multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of
 # these multiples is 23. Find the sum of all the multiples
 # of 3 or 5 below 1000.
-
+###############################################################################
 
 # Determine multiples of 3 & 5 below a max value via a while loop and add them
 # to list.
@@ -68,3 +70,11 @@ def sum_multiples_of_3_or_5_using_two_ranges(end):
 
 total = sum_multiples_of_3_or_5_using_two_ranges(1000)
 print "sum_multiples_of_3_or_5_using_two_ranges: %d" % total
+
+
+# One liner
+def sum_multiples_of_3_or_5_one_liner(end):
+	return sum([x for x in range(end) if x % 3== 0 or x % 5== 0])
+
+total = sum_multiples_of_3_or_5_one_liner(1000)
+print "sum_multiples_of_3_or_5_one_liner: %d" % total
