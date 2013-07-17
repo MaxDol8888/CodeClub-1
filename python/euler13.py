3b4789d4df3747833eaae1bea4b8adef893b278f
Casey Rubabno <casey.rubano@gmail.com>
2013-07-17 11:58:27 -0700

    Updated euler 13 solution to have 2 other solutions with different approaches

--- python/euler13.py
+++ python/euler13.py
@@ -11,15 +11,46 @@
 import time
 
 def calculate_sum(filename):
-	sum = 0
+	
+	total = 0
 
 	for line in open(filename).readlines():
-		sum += int(line)
+		total += int(line)
 	
-	return sum
+	return total
 
 start = time.time()
 for i in range(1000): result = str(calculate_sum("euler13.txt"))[:10]
 elapsed = (time.time() - start)
 print ("result %s returned after %s seconds (1000 iterations)." % (result, elapsed))
-#result 5537376230 returned after 0.19401121139526367 seconds (1000 iterations).
+#result 5537376230 returned after 0.19501090049743652 seconds (1000 iterations).
+
+
+
+def calculate_sum_using_array(filename):
+	
+	numbers = open(filename).readlines()
+
+	numbers = []
+	for line in open(filename).readlines():
+		numbers.append(int(line))
+	
+	return sum(numbers)
+
+start = time.time()
+for i in range(1000): result = str(calculate_sum_using_array("euler13.txt"))[:10]
+elapsed = (time.time() - start)
+print ("result %s returned after %s seconds (1000 iterations)." % (result, elapsed))
+#result 5537376230 returned after 0.33101892471313477 seconds (1000 iterations).
+
+
+
+def calculate_sum_using_lambda(filename):
+	
+	return sum(map(lambda x: int(x), open(filename).readlines()))
+
+start = time.time()
+for i in range(1000): result = str(calculate_sum_using_lambda("euler13.txt"))[:10]
+elapsed = (time.time() - start)
+print ("result %s returned after %s seconds (1000 iterations)." % (result, elapsed))
+#result 5537376230 returned after 0.21001219749450684 seconds (1000 iterations).
